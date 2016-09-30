dde76be568ad972645ce42430f4a9a3a27d4f5b5
omura.522 <omura.522@fssoftware.co.jp>
2016-09-30 18:02:41 +0900

    Add Rectangle

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -7,20 +7,35 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Rectangle
+import com.badlogic.gdx.utils.TimeUtils
+import java.util.*
 
 class MyGdxGame : ScreenAdapter() {
     private lateinit var batch: SpriteBatch
     private lateinit var balls: List<Sprite>
     private lateinit var assetManager: AssetManager
+    private lateinit var balldrops: ArrayList<Rectangle>
+    private var lastDropTime: Long = 0
 
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
         balls[0].draw(batch)
+//        for (balldrop in balldrops)
+//        {
+//            balls[0].setPosition(balldrop.x, balldrop.y)
+//            balls[0].setScale(0.5f)
+//            balls[0].draw(batch)
+//        }
         batch.end()
 
         balls[0].translate(0.1f, 0f)
+
+        // check if we need to create a new raindrop
+        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBallDrop();
 
     }
 
@@ -29,6 +44,11 @@ class MyGdxGame : ScreenAdapter() {
         batch = SpriteBatch()
         createBalls()
         balls[0].setPosition(100f, 100f)
+        balls[0].setScale(0.5f)
+
+        // create the balldrops ArrayList and spawn the first balldrop
+        balldrops = ArrayList<Rectangle>()
+        spawnBallDrop()
     }
 
     override fun dispose() {
@@ -61,5 +81,14 @@ class MyGdxGame : ScreenAdapter() {
                                       ball_ff0000,
                                       ball_ff0064,
                                       ball_ffffff)
+    }
+
+    private fun spawnBallDrop() {
+        val balldrop = Rectangle()
+        balldrop.x = MathUtils.random(0f, 800f - 64f)
+        print("balldrop.x:${balldrop.x}")
+        balldrop.y = 50f
+        balldrops.add(balldrop)
+        lastDropTime = TimeUtils.nanoTime()
     }
 }
