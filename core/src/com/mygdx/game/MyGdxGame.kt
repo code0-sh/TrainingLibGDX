ac6827e440fd340a16e536d9fe7049ab91b3a150
omura.522 <omura.522@fssoftware.co.jp>
2016-09-30 13:29:41 +0900

    Add ball

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -1,26 +1,61 @@
 package com.mygdx.game
 
-
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 
 class MyGdxGame : ScreenAdapter() {
     private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+    private lateinit var balls: List<TextureRegion>
+    private lateinit var assetManager: AssetManager
 
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        batch.draw(balls[0], 100f, 100f)
         batch.end()
     }
 
     override fun show() {
+        loadResource()
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        createBalls()
+    }
+
+    override fun dispose() {
+        assetManager.dispose()
+    }
+
+    private fun loadResource() {
+        assetManager = AssetManager()
+        assetManager.load("balls.txt", TextureAtlas::class.java)
+        assetManager.finishLoading()
+    }
+
+    private fun createBalls() {
+        val atlas = assetManager.get("balls.txt", TextureAtlas::class.java)
+
+        val ball_0000ff = atlas.findRegion("0000ff")
+        val ball_00ff00 = atlas.findRegion("00ff00")
+        val ball_00ffff = atlas.findRegion("00ffff")
+        val ball_0300ff = atlas.findRegion("0300ff")
+        val ball_3200ff = atlas.findRegion("3200ff")
+        val ball_ff0000 = atlas.findRegion("ff0000")
+        val ball_ff0064 = atlas.findRegion("ff0064")
+        val ball_ffffff = atlas.findRegion("ffffff")
+
+        balls = listOf<TextureRegion>(ball_0000ff,
+                                      ball_00ff00,
+                                      ball_00ffff,
+                                      ball_0300ff,
+                                      ball_3200ff,
+                                      ball_ff0000,
+                                      ball_ff0064,
+                                      ball_ffffff)
     }
 }
