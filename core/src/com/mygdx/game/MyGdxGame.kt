b5f7ecdfccdaa5efb116a2d72c32bacb1d053f0e
omura.522 <omura.522@fssoftware.co.jp>
2016-09-30 15:11:06 +0900

    Add func translate

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -4,27 +4,31 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 
 class MyGdxGame : ScreenAdapter() {
     private lateinit var batch: SpriteBatch
-    private lateinit var balls: List<TextureRegion>
+    private lateinit var balls: List<Sprite>
     private lateinit var assetManager: AssetManager
 
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
-        batch.draw(balls[0], 100f, 100f)
+        balls[0].draw(batch)
         batch.end()
+
+        balls[0].translate(0.1f, 0f)
+
     }
 
     override fun show() {
         loadResource()
         batch = SpriteBatch()
         createBalls()
+        balls[0].setPosition(100f, 100f)
     }
 
     override fun dispose() {
@@ -40,16 +44,16 @@ class MyGdxGame : ScreenAdapter() {
     private fun createBalls() {
         val atlas = assetManager.get("balls.txt", TextureAtlas::class.java)
 
-        val ball_0000ff = atlas.findRegion("0000ff")
-        val ball_00ff00 = atlas.findRegion("00ff00")
-        val ball_00ffff = atlas.findRegion("00ffff")
-        val ball_0300ff = atlas.findRegion("0300ff")
-        val ball_3200ff = atlas.findRegion("3200ff")
-        val ball_ff0000 = atlas.findRegion("ff0000")
-        val ball_ff0064 = atlas.findRegion("ff0064")
-        val ball_ffffff = atlas.findRegion("ffffff")
+        val ball_0000ff = Sprite(atlas.findRegion("0000ff"))
+        val ball_00ff00 = Sprite(atlas.findRegion("00ff00"))
+        val ball_00ffff = Sprite(atlas.findRegion("00ffff"))
+        val ball_0300ff = Sprite(atlas.findRegion("0300ff"))
+        val ball_3200ff = Sprite(atlas.findRegion("3200ff"))
+        val ball_ff0000 = Sprite(atlas.findRegion("ff0000"))
+        val ball_ff0064 = Sprite(atlas.findRegion("ff0064"))
+        val ball_ffffff = Sprite(atlas.findRegion("ffffff"))
 
-        balls = listOf<TextureRegion>(ball_0000ff,
+        balls = listOf<Sprite>(ball_0000ff,
                                       ball_00ff00,
                                       ball_00ffff,
                                       ball_0300ff,
