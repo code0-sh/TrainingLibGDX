20cd0f4592e1c5fa68a1f692719bc6fbfb4a55b0
omura.522 <omura.522@fssoftware.co.jp>
2016-10-03 12:28:01 +0900

    Add touch event

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -1,54 +1,66 @@
 package com.mygdx.game
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
-import com.badlogic.gdx.math.MathUtils
-import com.badlogic.gdx.math.Rectangle
-import com.badlogic.gdx.utils.TimeUtils
-import java.util.*
 
 class MyGdxGame : ScreenAdapter() {
     private lateinit var batch: SpriteBatch
-    private lateinit var balls: List<Sprite>
+    private lateinit var balls: List<TextureAtlas.AtlasRegion>
     private lateinit var assetManager: AssetManager
-    private lateinit var balldrops: ArrayList<Rectangle>
-    private var lastDropTime: Long = 0
+    private lateinit var ball: Sprite
+    var screenWidth: Float = 0f
+    var screenHeight: Float = 0f
+    var ballWidth: Float = 0f
+    var ballHeight: Float = 0f
+    private lateinit var camera: OrthographicCamera
 
     override fun render(delta: Float) {
         Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         batch.begin()
-        balls[0].draw(batch)
-//        for (balldrop in balldrops)
-//        {
-//            balls[0].setPosition(balldrop.x, balldrop.y)
-//            balls[0].setScale(0.5f)
-//            balls[0].draw(batch)
-//        }
+        ball.draw(batch)
         batch.end()
 
-        balls[0].translate(0.1f, 0f)
-
-        // check if we need to create a new raindrop
-        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnBallDrop();
-
+        ball.translate(0.1f, 0f)
     }
 
     override fun show() {
         loadResource()
-        batch = SpriteBatch()
-        createBalls()
-        balls[0].setPosition(100f, 100f)
-        balls[0].setScale(0.5f)
 
-        // create the balldrops ArrayList and spawn the first balldrop
-        balldrops = ArrayList<Rectangle>()
-        spawnBallDrop()
+        screenWidth = Gdx.graphics.width.toFloat()
+        screenHeight = Gdx.graphics.height.toFloat()
+        camera = OrthographicCamera(screenWidth, screenHeight)
+        camera.position.set(screenWidth / 2, screenHeight / 2, 0f)
+        camera.update()
+
+        batch = SpriteBatch()
+
+        createBalls()
+
+        ball = Sprite(balls[0])
+        ball.setPosition(10f, 10f)
+        ball.setScale(0.5f)
+
+        ballWidth = ball.width
+        ballHeight = ball.height
+        ball.setBounds( camera.position.x - (ballWidth / 2), camera.position.y - (ballHeight / 2), ballWidth, ballHeight)
+
+        Gdx.input.inputProcessor = object: InputAdapter() {
+            override fun touchDown(screenX:Int, screenY:Int, pointer:Int, button:Int):Boolean {
+                if (ball.boundingRectangle.contains(screenX.toFloat(), screenY.toFloat())) {
+                    println("Ball Clicked")
+                }
+                return true
+            }
+        }
     }
 
     override fun dispose() {
@@ -61,34 +73,26 @@ class MyGdxGame : ScreenAdapter() {
         assetManager.finishLoading()
     }
 
-    private fun createBalls() {
+    private fun createBalls(): List<TextureAtlas.AtlasRegion> {
         val atlas = assetManager.get("balls.txt", TextureAtlas::class.java)
 
-        val ball_0000ff = Sprite(atlas.findRegion("0000ff"))
-        val ball_00ff00 = Sprite(atlas.findRegion("00ff00"))
-        val ball_00ffff = Sprite(atlas.findRegion("00ffff"))
-        val ball_0300ff = Sprite(atlas.findRegion("0300ff"))
-        val ball_3200ff = Sprite(atlas.findRegion("3200ff"))
-        val ball_ff0000 = Sprite(atlas.findRegion("ff0000"))
-        val ball_ff0064 = Sprite(atlas.findRegion("ff0064"))
-        val ball_ffffff = Sprite(atlas.findRegion("ffffff"))
+        val ball_0000ff = atlas.findRegion("0000ff")
+        val ball_00ff00 = atlas.findRegion("00ff00")
+        val ball_00ffff = atlas.findRegion("00ffff")
+        val ball_0300ff = atlas.findRegion("0300ff")
+        val ball_3200ff = atlas.findRegion("3200ff")
+        val ball_ff0000 = atlas.findRegion("ff0000")
+        val ball_ff0064 = atlas.findRegion("ff0064")
+        val ball_ffffff = atlas.findRegion("ffffff")
 
-        balls = listOf<Sprite>(ball_0000ff,
-                                      ball_00ff00,
-                                      ball_00ffff,
-                                      ball_0300ff,
-                                      ball_3200ff,
-                                      ball_ff0000,
-                                      ball_ff0064,
-                                      ball_ffffff)
-    }
-
-    private fun spawnBallDrop() {
-        val balldrop = Rectangle()
-        balldrop.x = MathUtils.random(0f, 800f - 64f)
-        print("balldrop.x:${balldrop.x}")
-        balldrop.y = 50f
-        balldrops.add(balldrop)
-        lastDropTime = TimeUtils.nanoTime()
+        balls = listOf<TextureAtlas.AtlasRegion>(ball_0000ff,
+                                                 ball_00ff00,
+                                                 ball_00ffff,
+                                                 ball_0300ff,
+                                                 ball_3200ff,
+                                                 ball_ff0000,
+                                                 ball_ff0064,
+                                                 ball_ffffff)
+        return balls
     }
 }
